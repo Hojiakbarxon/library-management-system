863e07dacd87a2bb98fbaee3392f754635c05a7d
Hojiakbarxon Olimxo'jayev <olimxojayev22.2007@gmail.com>
2026-06-13 14:57:21 +0500

    Fix the logical bug, where creating borrow request and serve uploaded book cover images via static assets

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,16 @@ import { envConfig } from './config/env.config';
 import { HttpStatus, ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 import { GlobalFilter } from './common/filters/global/global.filter';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.setGlobalPrefix('api');
   app.use(cookieParser());
+
+  app.useStaticAssets(join(process.cwd(), 'uploads'), { prefix: '/api/uploads' });
 
   app.useGlobalFilters(new GlobalFilter())
 
@@ -34,7 +38,7 @@ async function bootstrap() {
   );
 
   app.enableCors({ origin: true, credentials: true })
-  
+
   await app.listen(envConfig.port, () =>
     console.log(`server is running on port `, envConfig.port),
   );
